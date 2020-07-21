35f72b8738e1d10dec7e4977be270096bc0fd20d
kongfl888 K <kongfl888@outlook.com>
2020-07-21 16:56:29 +0800

    Update merge

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -4,5 +4,5 @@ pdir=/tmp/patches-r2s
 git clone -b test --single-branch --depth 1 https://github.com/kongfl888/friendlywrt.git $pdir
 
 for file in $pdir/r2s-patches-new/*; do
-    git apply --check $file && git am $file || echo ""
+    git am --whitespace=fix $file || git am --abort
 done
