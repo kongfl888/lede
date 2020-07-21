9f8e0daa3f25d8d22ca4488ac36ef68351d1ede7
kongfl888 K <kongfl888@outlook.com>
2020-07-21 13:27:28 +0800

    Update test

---
+++ scripts/apply.sh
@@ -0,0 +1,8 @@
+
+pdir=/tmp/patches-r2s
+
+git clone -b test --single-branch --depth 1 https://github.com/kongfl888/friendlywrt.git $pdir
+
+for file in $pdir/*; do
+    git apply --check $file && git am $file || echo ""
+done
